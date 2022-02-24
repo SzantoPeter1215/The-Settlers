2dd6a8a1cf33920010c2041e8678e2b58d1cc7fe
Tóth Márton <ejjb7g@inf.elte.hu>
2022-02-24 11:19:04 +0100

    PathFinder added for testing

---
+++ src/PathFinder.java
@@ -0,0 +1,173 @@
+import java.util.*;
+
+// A queue node used in BFS
+class Node
+{
+    // (x, y) represents coordinates of a cell in the matrix
+    int x, y;
+
+    // maintain a parent node for printing the final path
+    Node parent;
+
+    Node(int x, int y, Node parent)
+    {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ')';
+    }
+}
+
+class PathFinder
+{
+    // Below arrays detail all four possible movements from a cell
+    private static int[] row = { -1, 0, 0, 1 };
+    private static int[] col = { 0, -1, 1, 0 };
+
+    // The function returns false if (x, y) is not a valid position
+    private static boolean isValid(int x, int y, int N) {
+        return (x >= 0 && x < N) && (y >= 0 && y < N);
+    }
+
+    // Utility function to find path from source to destination
+    private static void findPath(Node node, List<String> path)
+    {
+        if (node != null) {
+            findPath(node.parent, path);
+            path.add(node.toString());
+        }
+    }
+
+    // Find the shortest route in a matrix from source cell (x, y) to
+    // destination cell (N-1, N-1)
+    public static List<String> findPath(int[][] matrix, int x, int y, int destX, int destY)
+    {
+        // list to store shortest path
+        List<String> path = new ArrayList<>();
+
+        // base case
+        if (matrix == null || matrix.length == 0) {
+            return path;
+        }
+
+        // `N × N` matrix
+        int N = matrix.length;
+
+        // create a queue and enqueue the first node
+        Queue<Node> q = new ArrayDeque<>();
+        Node src = new Node(x, y, null);
+        q.add(src);
+
+        // set to check if the matrix cell is visited before or not
+        Set<String> visited = new HashSet<>();
+
+        String key = src.x + "," + src.y;
+        visited.add(key);
+
+        // loop till queue is empty
+        while (!q.isEmpty())
+        {
+            // dequeue front node and process it
+            Node curr = q.poll();
+            int i = curr.x, j = curr.y;
+
+            // return if the destination is found
+            if (i == destX && j == destY) {
+                findPath(curr, path);
+                return path;
+            }
+
+            // value of the current cell
+            int n = matrix[i][j];
+
+            // check all four possible movements from the current cell
+            // and recur for each valid movement
+            for (int k = 0; k < row.length; k++)
+            {
+                // get next position coordinates using the value of the current cell
+                x = i + row[k] * n;
+                y = j + col[k] * n;
+
+                // check if it is possible to go to the next position
+                // from the current position
+                if (isValid(x, y, N))
+                {
+                    // construct the next cell node
+                    Node next = new Node(x, y, curr);
+
+                    key = next.x + "," + next.y;
+
+                    // if it isn't visited yet
+                    if (!visited.contains(key))
+                    {
+                        // enqueue it and mark it as visited
+                        q.add(next);
+                        visited.add(key);
+                    }
+                }
+            }
+        }
+
+        // we reach here if the path is not possible
+        return path;
+    }
+
+    public static void main(String[] args)
+    {
+        int[][] matrix =
+                {
+                        { 1, 1, 1, 1, 1, 0, 2, 2, 2, 2 },
+                        { 1, 1, 1, 1, 1, 0, 2, 2, 2, 2 },
+                        { 1, 1, 1, 1, 1, 0, 2, 2, 2, 2 },
+                        { 1, 1, 1, 1, 1, 0, 2, 2, 2, 2 },
+                        { 1, 1, 1, 1, 1, 0, 2, 2, 2, 2 },
+                        { 3, 3, 1, 1, 1, 0, 0, 0, 0, 0 },
+                        { 3, 3, 3, 3, 3, 0, 1, 1, 1, 1 },
+                        { 3, 3, 3, 3, 3, 0, 1, 1, 1, 1 },
+                        { 3, 3, 3, 3, 3, 0, 1, 1, 1, 1 },
+                        { 3, 3, 3, 3, 3, 0, 1, 1, 1, 1 }
+                };
+
+        // Find a route in the matrix from source cell (0, 0) to
+        // destination cell (N-1, N-1)
+        List<String> path = findPath(matrix, 0, 0, 9, 9);
+
+        if (path != null && path.size() > 0) {
+            System.out.print("The shortest path is " + path);
+
+            int pathSize = path.size();
+
+            int[] XChords = new int[pathSize];
+            int[] YChords = new int[pathSize];
+
+            for(int i = 0; i < pathSize; ++i) {
+                String[] raw = path.get(i).split(", ");
+                XChords[i] = Integer.parseInt(String.valueOf(raw[0].charAt(1)));
+                YChords[i] = Integer.parseInt(String.valueOf(raw[1].charAt(0)));
+            }
+            System.out.print("\n\n");
+            for(int x = 0; x < matrix.length; ++x) {
+                for(int y = 0; y < matrix.length; ++y) {
+                    boolean found = false;
+                    for(int p = 0; p < pathSize; ++p) {
+                        if(XChords[p] == x && YChords[p] == y) {
+                            System.out.print(" # ");
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(!found) {
+                        System.out.print(" · ");
+                    }
+                }
+                System.out.print("\n");
+            }
+        } else {
+            System.out.println("Destination is not found");
+        }
+    }
+}
