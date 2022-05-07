3ee6483e2df1a2ad91e4a32063bfe64f9c95bd7b
Tóth Márton <ejjb7g@inf.elte.hu>
2022-05-07 20:32:37 +0200

    reverting the multiple commits

--- src/test/java/hu/elte/fi/szofttech/pacman/model/GameLogicTest.java
+++ src/test/java/hu/elte/fi/szofttech/pacman/model/GameLogicTest.java
@@ -4,7 +4,6 @@ import main.java.hu.elte.fi.szofttech.pacman.gui.GameUIConstants;
 import main.java.hu.elte.fi.szofttech.pacman.model.GameLogic;
 import main.java.hu.elte.fi.szofttech.pacman.model.PlayerTurn;
 
-import java.io.IOException;
 
 //import org.junit.jupiter.api.Test;
 //import static org.junit.jupiter.api.Assertions.*;
@@ -12,16 +11,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-class GameLogicTest  {
+class GameLogicTest {
     @Test
-    public void testMoney() throws IOException {
+    public void testNewGame(){
+        GameLogic gameLogic = new GameLogic();
+        GameUIConstants gameConstants = new GameUIConstants();
+        //gameLogic.newGame(gameConstants.GAMEAREA_HEIGHT_canBeDividedBy,gameConstants.GAMEAREA_WIDTH_canBeDividedBy,"asd");
+        //assertEquals(gameConstants.GAMEAREA_HEIGHT_canBeDividedBy, gameLogic.getRow());
+        //assertEquals(gameConstants.GAMEAREA_WIDTH_canBeDividedBy, gameLogic.getColumn());
+        //assertEquals(null, gameLogic.getGrids());
+    }
+
+    @Test
+    public void testMoney(){
         GameLogic gameLogic = new GameLogic();
         assertEquals(1000, gameLogic.getPlayer1Gold());
         assertEquals(1000, gameLogic.getPlayer2Gold());
     }
 
     @Test
-    public void testRemoveMoney() throws IOException {
+    public void testRemoveMoney(){
         GameLogic gameLogic = new GameLogic();
         gameLogic.removeMoney(100, PlayerTurn.PLAYER1);
         assertEquals(900, gameLogic.getPlayer1Gold());
@@ -30,7 +39,7 @@ class GameLogicTest  {
     }
 
     @Test
-    public void testAddMoney() throws IOException {
+    public void testAddMoney(){
         GameLogic gameLogic = new GameLogic();
         gameLogic.addMoney(100, 1);
         assertEquals(1100, gameLogic.getPlayer1Gold());
