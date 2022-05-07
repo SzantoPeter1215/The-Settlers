8c5570b47748a420eca4de0c750fd2ce3169fdeb
Tóth Márton <ejjb7g@inf.elte.hu>
2022-05-07 16:05:59 +0200

    junit test replace 2

--- src/model/dijkstra/GraphUtils.java
+++ src/model/dijkstra/GraphUtils.java
@@ -1,6 +1,7 @@
 package model.dijkstra;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
 
 import model.Field;
 
