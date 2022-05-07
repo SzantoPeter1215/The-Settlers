91474d470f39cde8de727ea6a29971d84db9f3d3
Tóth Márton <ejjb7g@inf.elte.hu>
2022-05-07 20:36:04 +0200

    MY TRULY finyl CI push

---
+++ src/test/java/hu/elte/fi/szofttech/pacman/model/InitTest.java
@@ -0,0 +1,19 @@
+package test.java.hu.elte.fi.szofttech.pacman.model;
+
+import main.java.hu.elte.fi.szofttech.pacman.gui.GameUIConstants;
+import main.java.hu.elte.fi.szofttech.pacman.model.GameLogic;
+import main.java.hu.elte.fi.szofttech.pacman.model.PlayerTurn;
+
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+class InitTest {
+    @Test
+    public void testNewGame() {
+
+        assertEquals(1, 1);
+    }
+}
