a8b40de9d4d9f5311256ba8e3ef766b30f076061
Tóth Márton <ejjb7g@inf.elte.hu>
2022-05-07 20:06:22 +0200

    CI FINISH 2

---
+++ src/test/java/hu/elte/fi/szofttech/pacman/model/BootTest.java
@@ -0,0 +1,16 @@
+package test.java.hu.elte.fi.szofttech.pacman.model;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+
+
+class BootTest  {
+    @Test
+    public void test1() throws IOException {
+        assertEquals(1, 1);
+    }
+
+}
--- src/test/java/hu/elte/fi/szofttech/pacman/model/GameLogicTest.java
+++ src/test/java/hu/elte/fi/szofttech/pacman/model/GameLogicTest.java
@@ -4,23 +4,24 @@ import main.java.hu.elte.fi.szofttech.pacman.gui.GameUIConstants;
 import main.java.hu.elte.fi.szofttech.pacman.model.GameLogic;
 import main.java.hu.elte.fi.szofttech.pacman.model.PlayerTurn;
 
+import java.io.IOException;
 
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
 
 
-class GameLogicTest {
+class GameLogicTest  {
     @Test
-    public void testMoney(){
+    public void testMoney() throws IOException {
         GameLogic gameLogic = new GameLogic();
         assertEquals(1000, gameLogic.getPlayer1Gold());
         assertEquals(1000, gameLogic.getPlayer2Gold());
     }
 
     @Test
-    public void testRemoveMoney(){
+    public void testRemoveMoney() throws IOException {
         GameLogic gameLogic = new GameLogic();
         gameLogic.removeMoney(100, PlayerTurn.PLAYER1);
         assertEquals(900, gameLogic.getPlayer1Gold());
@@ -29,7 +30,7 @@ class GameLogicTest {
     }
 
     @Test
-    public void testAddMoney(){
+    public void testAddMoney() throws IOException {
         GameLogic gameLogic = new GameLogic();
         gameLogic.addMoney(100, 1);
         assertEquals(1100, gameLogic.getPlayer1Gold());
